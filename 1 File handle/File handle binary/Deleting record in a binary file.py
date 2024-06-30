c9dd7fbeee5e2dd877b72376d92580a37d09f7c1
xdffish <xdffish@qq.com>
2024-06-30 13:06:40 +0800

    更新一下

--- 1 File handle/File handle binary/Deleting record in a binary file.py
+++ 1 File handle/File handle binary/Deleting record in a binary file.py
@@ -1,17 +1,17 @@
 import pickle
-
+import os
+print(os.getcwd())
 
 def bdelete():
-    # Opening a file & loading it
-    with open("studrec.dat") as F:
+    # 打开文件并加载数据
+    with open("1 File handle/File handle binary/studrec.dat", "rb") as F:
         stud = pickle.load(F)
         print(stud)
 
-    # Deleting the Roll no. entered by user
-    rno = int(input("Enter the Roll no. to be deleted: "))
-    with open("studrec.dat") as F:
+    # 删除用户输入的学号
+    rno = int(input("请输入要删除的学号："))
+    with open("1 File handle/File handle binary/studrec.dat", "wb") as F:
         rec = [i for i in stud if i[0] != rno]
         pickle.dump(rec, F)
-
 
 bdelete()
--- 1 File handle/File handle binary/question 1 (elegible for remedial, top marks).py
+++ 1 File handle/File handle binary/question 1 (elegible for remedial, top marks).py
@@ -1,18 +1,15 @@
-"""Amit is a monitor of class XII-A and he stored the record of all
-the students of his class in a file named “class.dat”.
-Structure of record is [roll number, name, percentage]. His computer
-teacher has assigned the following duty to Amit
+"""Amit 是十二年级A班的班长，他将班上所有学生的记录存储在一个名为“class.dat”的文件中。
+记录的结构是[学号, 姓名, 成绩百分比]。他的计算机老师分配给Amit以下任务：
 
-Write a function remcount( ) to count the number of students who need
- remedial class (student who scored less than 40 percent)
+编写一个函数 remcount() 来计算需要补习课的学生数量（成绩低于40%的学生）
 
- 
  """
-# also find no. of children who got top marks
+# 同时找出获得最高分的学生数量
 
 import pickle
 
-list = [
+# 学生数据列表
+students_list = [
     [1, "Ramya", 30],
     [2, "vaishnavi", 60],
     [3, "anuya", 40],
@@ -23,46 +20,49 @@ list = [
     [8, "sandhya", 65],
 ]
 
-with open("class.dat", "ab") as F:
-    pickle.dump(list, F)
-    F.close()
+# 将学生数据序列化后存储到文件
+with open("class.dat", "ab") as file:
+    pickle.dump(students_list, file)
+    file.close()
 
-
+# 计算需要补习的学生数量
 def remcount():
-    with open("class.dat", "rb") as F:
-        val = pickle.load(F)
-        count = 0
+    with open("class.dat", "rb") as file:
+        students_data = pickle.load(file)
+        remedial_count = 0
 
-        for i in val:
-            if i[2] <= 40:
-                print(f"{i} eligible for remedial")
-                count += 1
-        print(f"the total number of students are {count}")
-
+        # 遍历学生数据，统计成绩低于或等于40%的学生
+        for student in students_data:
+            if student[2] <= 40:
+                print(f"{student} eligible for remedial")
+                remedial_count += 1
+        print(f"the total number of students are {remedial_count}")
 
 remcount()
 
-
+# 查找获得最高分的学生
 def firstmark():
-    with open("class.dat", "rb") as F:
-        val = pickle.load(F)
-        count = 0
-        main = [i[2] for i in val]
+    with open("class.dat", "rb") as file:
+        students_data = pickle.load(file)
+        top_marks_count = 0
+        scores = [student[2] for student in students_data]
 
-        top = max(main)
-        print(top, "is the first mark")
+        # 获取最高分
+        highest_score = max(scores)
+        print(highest_score, "is the first mark")
 
-        F.seek(0)
-        for i in val:
-            if top == i[2]:
-                print(f"{i}\ncongrats")
-                count += 1
+        file.seek(0)
+        # 遍历学生数据，找出获得最高分的学生
+        for student in students_data:
+            if highest_score == student[2]:
+                print(f"{student}\ncongrats")
+                top_marks_count += 1
 
-        print("the total number of students who secured top marks are", count)
-
+        print("the total number of students who secured top marks are", top_marks_count)
 
 firstmark()
 
-with open("class.dat", "rb") as F:
-    val = pickle.load(F)
-    print(val)
+# 读取并打印所有学生数据
+with open("class.dat", "rb") as file:
+    all_students_data = pickle.load(file)
+    print(all_students_data)
